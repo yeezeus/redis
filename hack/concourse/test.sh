0fb031e2a038284a693f4f213cc34730e6b0f5b2
Tamal Saha <tamal@appscode.com>
2018-07-08 12:47:47 -0700

    Format shell script (#39)
    find hack | grep sh | xargs shfmt -l -w -ci -i 2 {}

--- hack/concourse/test.sh
+++ hack/concourse/test.sh
@@ -8,18 +8,18 @@ docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker run hello-world
 
 # install dependencies
-apt-get update > /dev/null
-apt-get install -y python python-pip > /dev/null
+apt-get update >/dev/null
+apt-get install -y python python-pip >/dev/null
 
 # install kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl &> /dev/null
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl &>/dev/null
 chmod +x ./kubectl
 mv ./kubectl /bin/kubectl
 
 # install onessl
-curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-linux-amd64 \
-  && chmod +x onessl \
-  && mv onessl /usr/local/bin/
+curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-linux-amd64 &&
+  chmod +x onessl &&
+  mv onessl /usr/local/bin/
 
 # install pharmer
 pushd /tmp
@@ -28,19 +28,19 @@ chmod +x pharmer-linux-amd64
 mv pharmer-linux-amd64 /bin/pharmer
 popd
 
-function cleanup {
-    # delete cluster on exit
-    pharmer get cluster || true
-    pharmer delete cluster $NAME || true
-    pharmer get cluster || true
-    sleep 120 || true
-    pharmer apply $NAME || true
-    pharmer get cluster || true
+function cleanup() {
+  # delete cluster on exit
+  pharmer get cluster || true
+  pharmer delete cluster $NAME || true
+  pharmer get cluster || true
+  sleep 120 || true
+  pharmer apply $NAME || true
+  pharmer get cluster || true
 
-    # delete docker image on exit
-    curl -LO https://raw.githubusercontent.com/appscodelabs/libbuild/master/docker.py || true
-    chmod +x docker.py || true
-    ./docker.py del_tag kubedbci rd-operator $CUSTOM_OPERATOR_TAG
+  # delete docker image on exit
+  curl -LO https://raw.githubusercontent.com/appscodelabs/libbuild/master/docker.py || true
+  chmod +x docker.py || true
+  ./docker.py del_tag kubedbci rd-operator $CUSTOM_OPERATOR_TAG
 }
 trap cleanup EXIT
 
@@ -63,7 +63,7 @@ export DOCKER_REGISTRY=kubedbci
 popd
 
 #create credential file for pharmer
-cat > cred.json <<EOF
+cat >cred.json <<EOF
 {
         "token" : "$TOKEN"
 }
@@ -81,7 +81,7 @@ sleep 120
 kubectl get nodes
 
 # create storageclass
-cat > sc.yaml <<EOF
+cat >sc.yaml <<EOF
 apiVersion: storage.k8s.io/v1
 kind: StorageClass
 metadata:
--- hack/docker/rd-operator/make.sh
+++ hack/docker/rd-operator/make.sh
@@ -19,29 +19,29 @@ IMG=rd-operator
 DIST=$GOPATH/src/github.com/kubedb/redis/dist
 mkdir -p $DIST
 if [ -f "$DIST/.tag" ]; then
-    export $(cat $DIST/.tag | xargs)
+  export $(cat $DIST/.tag | xargs)
 fi
 
 clean() {
-    pushd $REPO_ROOT/hack/docker/rd-operator
-    rm -f rd-operator Dockerfile
-    popd
+  pushd $REPO_ROOT/hack/docker/rd-operator
+  rm -f rd-operator Dockerfile
+  popd
 }
 
 build_binary() {
-    pushd $REPO_ROOT
-    ./hack/builddeps.sh
-    ./hack/make.py build rd-operator
-    detect_tag $DIST/.tag
-    popd
+  pushd $REPO_ROOT
+  ./hack/builddeps.sh
+  ./hack/make.py build rd-operator
+  detect_tag $DIST/.tag
+  popd
 }
 
 build_docker() {
-    pushd $REPO_ROOT/hack/docker/rd-operator
-    cp $DIST/rd-operator/rd-operator-alpine-amd64 rd-operator
-    chmod 755 rd-operator
+  pushd $REPO_ROOT/hack/docker/rd-operator
+  cp $DIST/rd-operator/rd-operator-alpine-amd64 rd-operator
+  chmod 755 rd-operator
 
-    cat >Dockerfile <<EOL
+  cat >Dockerfile <<EOL
 FROM alpine
 
 RUN set -x \
@@ -52,40 +52,40 @@ COPY rd-operator /usr/bin/rd-operator
 USER nobody:nobody
 ENTRYPOINT ["rd-operator"]
 EOL
-    local cmd="docker build -t $DOCKER_REGISTRY/$IMG:$TAG ."
-    echo $cmd; $cmd
+  local cmd="docker build -t $DOCKER_REGISTRY/$IMG:$TAG ."
+  echo $cmd; $cmd
 
-    rm rd-operator Dockerfile
-    popd
+  rm rd-operator Dockerfile
+  popd
 }
 
 build() {
-    build_binary
-    build_docker
+  build_binary
+  build_docker
 }
 
 docker_push() {
-    if [ "$APPSCODE_ENV" = "prod" ]; then
-        echo "Nothing to do in prod env. Are you trying to 'release' binaries to prod?"
-        exit 1
-    fi
-    if [ "$TAG_STRATEGY" = "git_tag" ]; then
-        echo "Are you trying to 'release' binaries to prod?"
-        exit 1
-    fi
-    hub_canary
+  if [ "$APPSCODE_ENV" = "prod" ]; then
+    echo "Nothing to do in prod env. Are you trying to 'release' binaries to prod?"
+    exit 1
+  fi
+  if [ "$TAG_STRATEGY" = "git_tag" ]; then
+    echo "Are you trying to 'release' binaries to prod?"
+    exit 1
+  fi
+  hub_canary
 }
 
 docker_release() {
-    if [ "$APPSCODE_ENV" != "prod" ]; then
-        echo "'release' only works in PROD env."
-        exit 1
-    fi
-    if [ "$TAG_STRATEGY" != "git_tag" ]; then
-        echo "'apply_tag' to release binaries and/or docker images."
-        exit 1
-    fi
-    hub_up
+  if [ "$APPSCODE_ENV" != "prod" ]; then
+    echo "'release' only works in PROD env."
+    exit 1
+  fi
+  if [ "$TAG_STRATEGY" != "git_tag" ]; then
+    echo "'apply_tag' to release binaries and/or docker images."
+    exit 1
+  fi
+  hub_up
 }
 
 source_repo $@
