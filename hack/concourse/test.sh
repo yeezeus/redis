bd2047f13e6cf47c624b515451f4705a106f65a2
Md. Tahsin Rahman <tahsynx@gmail.com>
2018-08-17 11:18:09 +0600

    Add support for running tests on cncf cluster (#52)

--- hack/concourse/test.sh
+++ hack/concourse/test.sh
@@ -2,30 +2,30 @@
 
 set -eoux pipefail
 
+ORG_NAME=kubedb
 REPO_NAME=redis
 OPERATOR_NAME=rd-operator
+APP_LABEL=kubedb #required for `kubectl describe deploy -n kube-system -l app=$APP_LABEL`
+
+export APPSCODE_ENV=dev
+export DOCKER_REGISTRY=kubedbci
 
 # get concourse-common
 pushd $REPO_NAME
-git status
-git subtree pull --prefix hack/concourse/common https://github.com/kubedb/concourse-common.git master --squash -m 'concourse'
+git status # required, otherwise you'll get error `Working tree has modifications.  Cannot add.`. why?
+git subtree pull --prefix hack/libbuild https://github.com/appscodelabs/libbuild.git master --squash -m 'concourse'
 popd
 
-source $REPO_NAME/hack/concourse/common/init.sh
+source $REPO_NAME/hack/libbuild/concourse/init.sh
 
 pushd "$GOPATH"/src/github.com/kubedb/$REPO_NAME
 
 # build and push docker-image
 ./hack/builddeps.sh
-export APPSCODE_ENV=dev
-export DOCKER_REGISTRY=kubedbci
 
-./hack/docker/rd-operator/make.sh build
-./hack/docker/rd-operator/make.sh push
-popd
-
-pushd $GOPATH/src/github.com/kubedb/$REPO_NAME
+./hack/docker/$OPERATOR_NAME/make.sh build
+./hack/docker/$OPERATOR_NAME/make.sh push
 
 # run tests
-source ./hack/deploy/make.sh --docker-registry=kubedbci
+source ./hack/deploy/setup.sh --docker-registry=kubedbci
 ./hack/make.py test e2e --v=1 --storageclass=$StorageClass --selfhosted-operator=true --ginkgo.flakeAttempts=2
