b3a1559ce11ec825649d6f5d066bba5bfd77be8e
Tamal Saha <tamal@appscode.com>
2018-10-02 17:20:11 -0700

    Add validation webhook for Namespace deletion (#84)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -7,6 +7,7 @@ import (
 	hooks "github.com/appscode/kubernetes-webhook-util/admission/v1beta1"
 	admissionreview "github.com/appscode/kubernetes-webhook-util/registry/admissionreview/v1beta1"
 	"github.com/kubedb/apimachinery/pkg/admission/dormantdatabase"
+	"github.com/kubedb/apimachinery/pkg/admission/namespace"
 	"github.com/kubedb/apimachinery/pkg/admission/snapshot"
 	rdAdmsn "github.com/kubedb/redis/pkg/admission"
 	"github.com/kubedb/redis/pkg/controller"
@@ -102,6 +103,7 @@ func (c completedConfig) New() (*RedisServer, error) {
 		&rdAdmsn.RedisMutator{},
 		&snapshot.SnapshotValidator{},
 		&dormantdatabase.DormantDatabaseValidator{},
+		&namespace.NamespaceValidator{},
 	}
 	ctrl, err := c.OperatorConfig.New()
 	if err != nil {
