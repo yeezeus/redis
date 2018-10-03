791ea93c1faaf7c9b61394dcf5343302196b53a6
Tamal Saha <tamal@appscode.com>
2018-10-02 17:10:37 -0700

    Use FQDN for kube-apiserver in AKS (#83)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/cmds/server/start.go
+++ pkg/cmds/server/start.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net"
 
+	"github.com/appscode/kutil/tools/clientcmd"
 	"github.com/kubedb/redis/pkg/controller"
 	"github.com/kubedb/redis/pkg/server"
 	"github.com/spf13/pflag"
@@ -60,7 +61,7 @@ func (o RedisServerOptions) Config() (*server.RedisServerConfig, error) {
 	if err := o.RecommendedOptions.ApplyTo(serverConfig, server.Scheme); err != nil {
 		return nil, err
 	}
-	serverConfig.EnableMetrics = true
+	clientcmd.Fix(serverConfig.ClientConfig)
 
 	controllerConfig := controller.NewOperatorConfig(serverConfig.ClientConfig)
 	if err := o.ExtraOptions.ApplyTo(controllerConfig); err != nil {
